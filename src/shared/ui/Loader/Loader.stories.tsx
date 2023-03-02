a77a5cb576a8d727abaf23dad6c088eabef1ecfb
Anastasia Polozova <polozova.anastasi@gmail.com>
2023-03-02 12:37:16 +0300

    add storybook, stories and loki

---
+++ src/shared/ui/Loader/Loader.stories.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+
+import { ThemeDecorator } from 'shared/config/storybook/ThemeDecorator/ThemeDecorator';
+import { Theme } from 'app/providers/ThemeProvider';
+import { Loader } from './Loader';
+import { Light } from 'widgets/PageLoader/PageLoader.stories';
+
+export default {
+    title: 'shared/Loader',
+    component: Loader,
+    argTypes: {
+        backgroundColor: { control: 'color' },
+    },
+    args: {
+        to: '/',
+    },
+} as ComponentMeta<typeof Loader>;
+const Template: ComponentStory<typeof Loader> = (args) => <Loader {...args} />;
+
+export const Primary = Template.bind({});
+Primary.args = {};
+export const PrimaryDark = Template.bind({});
+PrimaryDark.args = {};
+PrimaryDark.decorators = [ThemeDecorator(Theme.DARK)];
